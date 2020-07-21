fd53d4dbb5d705773789fbe0f832dae5c2023460
Ramesh Pittala <ramesh.pittala@equinox.com>
2020-07-21 15:15:04 +0530

    changes added for source

---
+++ src/main/java/com/iamvickyav/springboot/SpringBootRestWithH2/SpringApp.java
@@ -0,0 +1,12 @@
+package com.iamvickyav.springboot.SpringBootRestWithH2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApp.class, args);
+	}
+}
